2eeacb25eb498e2657827bd1a91ffab22e00172b
rawansuww1010 <59528095+rawansuww1010@users.noreply.github.com>
2020-02-01 07:30:15 +0300

    Add files via upload

---
+++ Piano Capacitive Touch/Piano_Touch/Piano_Touch.ino
@@ -0,0 +1,75 @@
+void setup() {
+
+ Serial.begin(9600);      //begins serial monitor 
+ for (int i=0; i<=9; i++)
+ {
+ pinMode(i, INPUT);         //initialize all pins as input to read
+ }
+
+}
+
+void loop() 
+{
+
+                           
+if (digitalRead(2) == HIGH)   //if voltage detected at pin 2
+{ 
+ 
+Serial.println("a");
+delay(500);                 //delay is added to match
+
+}
+
+if (digitalRead(3) == HIGH)
+{
+Serial.println("s");
+delay(500);
+
+}
+
+if (digitalRead(4) == HIGH)
+{
+Serial.println("d");
+delay(500);
+
+}
+
+if (digitalRead(5) == HIGH)
+{
+Serial.println("f");
+delay(500);
+
+}
+
+if (digitalRead(6) == HIGH)
+{
+Serial.println("g");
+delay(500);
+
+}
+
+if (digitalRead(7) == HIGH)
+{
+Serial.println("h");
+delay(500);
+
+}
+if (digitalRead(8) == HIGH)
+{
+Serial.println("j");
+delay(500);
+
+}
+
+
+if (digitalRead(9) == HIGH)
+{
+  Serial.println("k");
+delay(500);
+
+}
+
+
+
+
+}
